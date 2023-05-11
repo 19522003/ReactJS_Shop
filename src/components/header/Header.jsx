ebf00d7109500cfb91ec21b8cd5f7fe99f5d40ab
Phat <19522003@gm.uit.edu.vn>
2023-05-11 17:16:13 +0700

    NEW

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -68,7 +68,7 @@ export default function Header() {
             </Button>
           )}
           {isLoggedIn && (
-            <IconButton>
+            <IconButton color="inherit">
               <AccountCircle />
             </IconButton>
           )}
